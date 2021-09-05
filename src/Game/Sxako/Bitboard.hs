45ae63d6fbcdc8cde3b29d4863b7a0c3430a2d90
Javran Cheng <Javran.C@gmail.com>
2021-09-04 19:53:24 -0700

    Read instance for Bitboard.

--- src/Game/Sxako/Bitboard.hs
+++ src/Game/Sxako/Bitboard.hs
@@ -5,17 +5,33 @@ module Game.Sxako.Bitboard
   )
 where
 
+import Control.Monad
 import Data.Bits
 import Data.List
 import Data.List.Split
 import Data.Word
+import Text.ParserCombinators.ReadP
 
 newtype Bitboard = Bitboard
   { getBitboard :: Word64
   }
-  deriving (Eq, Bits) via Word64
+  deriving (Eq, Bits, Bounded) via Word64
 
 instance Show Bitboard where
   show (Bitboard v) =
     intercalate "|" . reverse . chunksOf 8 $
       fmap (\i -> if testBit v i then '*' else '_') [0 .. 63]
+
+instance Read Bitboard where
+  readsPrec _ = readP_to_S $ do
+    let bitP = (True <$ char '*') <++ (False <$ char '_')
+        rankP = replicateM 8 bitP
+    x <- rankP
+    xs <- replicateM 7 (char '|' *> rankP)
+    let bits = concat . reverse $ x : xs
+    pure $
+      Bitboard $
+        foldr
+          (\(i, b) r -> if b then setBit r i else r)
+          0
+          (zip [0 .. 63] bits)

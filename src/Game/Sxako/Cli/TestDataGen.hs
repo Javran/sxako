f2d946fdea04f2b720b91025d3b2998367cb4663
Javran Cheng <Javran.C@gmail.com>
2021-09-11 19:38:59 -0700

    comm infra with stockfish.

--- src/Game/Sxako/Cli/TestDataGen.hs
+++ src/Game/Sxako/Cli/TestDataGen.hs
@@ -6,6 +6,10 @@
 
 module Game.Sxako.Cli.TestDataGen where
 
+import Control.Applicative
+import Control.Monad
+import Data.Function
+import Data.List
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
 import Data.Yaml
@@ -13,6 +17,8 @@ import Game.Sxako.Fen
 import Game.Sxako.Move
 import System.Environment
 import System.Exit
+import System.IO
+import System.Process.Typed
 
 data TestPayload
   = -- | all legal moves and their resulting positions should match.
@@ -44,11 +50,54 @@ subCmdMain :: String -> IO ()
 subCmdMain cmdHelpPrefix =
   getArgs >>= \case
     [inputFp] -> do
-      r <- decodeFileEither @[TestData] inputFp
-      print r
+      Right tests <- decodeFileEither @[TestData] inputFp
+      withStockfish $ \hIn hOut ->
+        forM_ tests $ \TestData {tdPosition} -> do
+          hPutStrLn hIn $ "position fen " <> show tdPosition
+          hPutStrLn hIn "go depth 1"
+          fix $ \loop -> do
+            raw <- hGetLine hOut
+            if "bestmove" `isPrefixOf` raw
+              then pure ()
+              else putStrLn raw >> loop
+          pure ()
     _ -> do
       putStrLn $ cmdHelpPrefix <> "<testdata>"
       exitFailure
 
-stockfishSetup :: IO ()
-stockfishSetup = pure () -- TODO
+withStockfish :: (Handle -> Handle -> IO r) -> IO r
+withStockfish f = do
+  let pc =
+        setStdout createPipe
+          . setStdin createPipe
+          $ proc "stockfish" []
+  putStrLn "Starting communication with stockfish ..."
+  withProcessWait pc $ \p -> do
+    let hIn = getStdin p
+        hOut = getStdout p
+        collectOptions :: Maybe Int -> IO (Maybe Int)
+        collectOptions m = do
+          -- get UCI options, for now we are only interested in max of MultiPV.
+          raw <- hGetLine hOut
+          if
+              | raw == "uciok" -> pure m
+              | "option name MultiPV" `isPrefixOf` raw ->
+                collectOptions
+                  (m <|> (Just . read . last . words $ raw))
+              | otherwise -> collectOptions m
+
+    hSetBuffering hIn LineBuffering
+    welcomeMsg <- hGetLine hOut
+    putStrLn $ "Stockfish: " <> welcomeMsg
+    hPutStrLn hIn "uci"
+    Just c <- collectOptions Nothing
+    putStrLn $ "MultiPV max value: " <> show c
+    hPutStrLn hIn $ "setoption name MultiPV value " <> show c
+    hPutStrLn hIn "isready"
+    do
+      raw <- hGetLine hOut
+      guard $ raw == "readyok"
+      putStrLn "Stockfish is ready."
+    result <- f hIn hOut
+    hPutStrLn hIn "quit"
+    pure result

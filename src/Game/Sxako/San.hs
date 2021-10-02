bcfd333954a442f80d9cb9bf3f6d3af90fbc26f7
Javran Cheng <Javran.C@gmail.com>
2021-10-01 23:08:09 -0700

    parsing castle notations first.

--- src/Game/Sxako/San.hs
+++ src/Game/Sxako/San.hs
@@ -1,9 +1,20 @@
-module Game.Sxako.San () where
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Game.Sxako.San
+  ( San (..)
+  , Disamb (..)
+  , CheckType (..)
+  , sanP
+  )
+where
 
 {-
   Short Algebraic Notation
  -}
 
+import Control.Applicative
+import Data.Attoparsec.ByteString.Char8 as Parser
 import Game.Sxako.Coord
 import Game.Sxako.Move
 import Game.Sxako.Types
@@ -28,3 +39,19 @@ data Disamb
   | DisambByCoord Coord
 
 data CheckType = Check | Checkmate
+
+sanP :: Parser San
+sanP = castleP
+  where
+    castleP = do
+      _ <- string "O-O"
+      mCh <- peekChar
+      sSide <- case mCh of
+        Just '-' -> QueenSide <$ string "-O"
+        _ -> pure KingSide
+      sCheck <-
+        option Nothing $
+          Just
+            <$> ((Check <$ char '+')
+                   <|> (Checkmate <$ char '#'))
+      pure SCastle {sSide, sCheck}

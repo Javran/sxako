76a967df7f0681b70b5fa3fe0a2aa97582183bde
Javran Cheng <Javran.C@gmail.com>
2021-10-13 23:02:58 -0700

    making some plans while reading PGN spec.

--- src/Game/Sxako/Pgn.hs
+++ src/Game/Sxako/Pgn.hs
@@ -6,6 +6,13 @@ where
 {-
   TODO: parsing PGN file.
 
+  Reference: http://www.saremba.de/chessgml/standards/pgn/pgn-complete.htm
+  - 18: Formal syntax
+
+  Import and export format: it appears that import format is just a lax form
+  of the export format, meaning that a parser accepting import format is sufficient
+  to handle export format without changes.
+
   We might need to break parsing down into two steps:
 
   - parsing the file literally.
@@ -20,5 +27,13 @@ where
   For a FEN record, we generate all plies in `Ply` datatype,
   and then come up with another type mapping all plies
   to short algebratic notation.
+
+  TODO: implementation plan.
+
+  - first round to recognize tag pairs and movetext section, without verification
+    + parsing tag pairs
+    + parsing movetext
+  - second round to turn this intermediate structure into a more organized one,
+    verification included.
 
  -}

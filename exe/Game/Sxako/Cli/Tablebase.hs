f6a679d97b73aaeb8f0481f8a5fc31698a020672
Javran Cheng <Javran.C@gmail.com>
2022-04-18 23:36:36 -0700

    matching result structure of tablebase.

---
+++ exe/Game/Sxako/Cli/Tablebase.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Game.Sxako.Cli.Tablebase where
+
+import GHC.Generics
+
+import Data.Aeson
+import qualified Data.Text as T
+
+-- reference: https://github.com/lichess-org/lila-tablebase
+data TbResult = TbResult
+  { dtz :: Maybe Int
+  , dtm :: Maybe Int
+  , checkmate :: Bool
+  , stalemate :: Bool
+  , insufficient_material :: Bool
+  , category :: T.Text
+  , moves :: [Value]
+  }
+  deriving (Generic, Show)
+
+instance FromJSON TbResult

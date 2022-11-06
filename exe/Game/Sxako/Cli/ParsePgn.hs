6ffc887f61851b220679e022ea6a5231f4936050
Javran Cheng <Javran.C@gmail.com>
2022-11-06 11:32:49 -0800

    allow parsing a list of pgn files

--- exe/Game/Sxako/Cli/ParsePgn.hs
+++ exe/Game/Sxako/Cli/ParsePgn.hs
@@ -13,40 +13,47 @@ import System.Exit
 import System.TimeIt (timeItT)
 import Text.Printf
 
+parseExperiment :: FilePath -> IO ()
+parseExperiment pgnFp = do
+  raw <- BS.readFile pgnFp
+  let parser = do
+        -- fuck BOM
+        _ <- option () (void (string "\239\187\191"))
+        ps <- manyPgnsP
+        leftover <- manyTill' anyChar endOfInput
+        pure (ps, leftover)
+  -- forcing input.
+  raw' <- evaluate $!! raw
+  -- forcing output.
+  (t, parsed) <- timeItT $ evaluate $!! Parser.parseOnly parser raw'
+  printf "Took %f seconds (CPU time)\n" t
+  case parsed of
+    Left msg ->
+      die $ "Failed when parsing: " <> msg
+    Right (r, leftover) ->
+      if null leftover
+        then do
+          printf "Parsed %d records, all succeeded.\n" (length r)
+        else do
+          printf
+            "Parsed %d records, with leftover length %d.\n"
+            (length r)
+            (length leftover)
+          case r of
+            [] -> pure ()
+            _ -> do
+              putStrLn "Last record:"
+              print $ last r
+          putStrLn "Start of leftover:"
+          putStrLn $ Prelude.take 100 leftover
+
 subCmdMain :: String -> IO ()
 subCmdMain cmdHelpPrefix =
   getArgs >>= \case
-    [pgnFp] -> do
-      raw <- BS.readFile pgnFp
-      let parser = do
-            -- fuck BOM
-            _ <- option () (void (string "\239\187\191"))
-            ps <- manyPgnsP
-            leftover <- manyTill' anyChar endOfInput
-            pure (ps, leftover)
-      -- forcing input.
-      raw' <- evaluate $!! raw
-      -- forcing output.
-      (t, parsed) <- timeItT $ evaluate $!! Parser.parseOnly parser raw'
-      case parsed of
-        Left msg ->
-          die $ "Failed when parsing: " <> msg
-        Right (r, leftover) ->
-          if null leftover
-            then do
-              printf "Parsed %d records in %s seconds (CPU time), all succeeded.\n" (length r) (show t)
-            else do
-              printf
-                "Parsed %d records, with leftover length %d.\n"
-                (length r)
-                (length leftover)
-              case r of
-                [] -> pure ()
-                _ -> do
-                  putStrLn "Last record:"
-                  print $ last r
-              putStrLn "Start of leftover:"
-              putStrLn $ Prelude.take 100 leftover
+    pgnFps | () == () -> do
+      forM_ pgnFps \pgnFp -> do
+        putStrLn $ "Processing " <> pgnFp <> " ..."
+        parseExperiment pgnFp
     _ -> do
       putStrLn $ cmdHelpPrefix <> "<PGN file>"
       exitFailure

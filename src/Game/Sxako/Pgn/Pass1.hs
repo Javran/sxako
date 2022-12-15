b12815c830890b0151138b634d39551ff5f0e172
Javran Cheng <Javran.C@gmail.com>
2022-12-14 21:41:40 -0800

    some progress implementing the stuff.

--- src/Game/Sxako/Pgn/Pass1.hs
+++ src/Game/Sxako/Pgn/Pass1.hs
@@ -1,5 +1,6 @@
 module Game.Sxako.Pgn.Pass1 where
 
+import Data.List
 import Data.Maybe
 import Game.Sxako.Pgn.Pass0
 import Game.Sxako.San
@@ -18,3 +19,40 @@ fromMovetextElem = \case
   MtSan s _ -> Just (Simp $ Left s)
   MtCommentary {} -> Nothing
   MtRav xs -> Just (Simp $ Right $ mapMaybe fromMovetextElem xs)
+
+newtype PlyNode a = PN (a, [PlyNode a])
+
+unreachable :: a
+unreachable = error "unreachable"
+
+parse :: forall a. [Simp a] -> Either String [PlyNode a]
+parse = \case
+  [] -> pure []
+  Simp x0 : xs0 -> case x0 of
+    Right _ -> Left "cannot start with RAV"
+    Left x1 -> do
+      let (rightsPre :: [Simp a], xs1) =
+            span
+              ( \case
+                  (Simp (Right _)) -> True
+                  (Simp (Left _)) -> False
+              )
+              xs0
+          rights :: [] [Simp a]
+          rights =
+            mapMaybe
+              ( \case
+                  (Simp (Left _)) -> unreachable
+                  (Simp (Right [])) -> Nothing
+                  (Simp (Right ys@(_ : _))) -> Just ys
+              )
+              rightsPre
+      {-
+        - x1, then xs1
+        - for r <- rights,
+          + head of r, then tail of r
+       -}
+      hdPns <- parse xs1
+      (rs :: [] [PlyNode a]) <- mapM parse rights
+      -- TODO: need to "flatten" this somehow.
+      pure $ PN (x1, hdPns) : fmap (error "TODO") rs

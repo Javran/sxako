bffba459dfc54cf1b1edc52ea499e0e7dbab6182
Javran Cheng <Javran.C@gmail.com>
2022-12-15 22:30:28 -0800

    proceed to have more smaller examples.

--- test/Game/Sxako/Pgn/Pass1Spec.hs
+++ test/Game/Sxako/Pgn/Pass1Spec.hs
@@ -7,6 +7,63 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "parse" do
+    let l ch = Simp (Left ch)
+        r xs = Simp (Right xs)
+        n = Node
+
+    specify "single line" do
+      parse [l 'A', l 'B', l 'C']
+        `shouldBe` Right
+          [ n 'A' [n 'B' [n 'C' []]]
+          ]
+
+    specify "alts from start" do
+      parse [l 'A', r [l 'B'], r [l 'C', l 'D']]
+        `shouldBe` Right
+          [ n 'A' []
+          , n 'B' []
+          , n 'C' [n 'D' []]
+          ]
+
+    specify "same tree different reps" do
+      {-
+        Turns out the PGN represnetation of ply-tree is not unique.
+        This unit test offers an example.
+       -}
+      let expect :: Tree Char
+          expect =
+            n
+              'A'
+              [ n 'B' []
+              , n 'C' []
+              , n 'D' []
+              , n 'E' []
+              ]
+      parse
+        [ l 'A'
+        , l 'B'
+        , r [l 'C']
+        , r [l 'D']
+        , r [l 'E']
+        ]
+        `shouldBe` Right [expect]
+
+      parse
+        [ l 'A'
+        , l 'B'
+        , r [l 'C', r [l 'D']]
+        , r [l 'E']
+        ]
+        `shouldBe` Right [expect]
+
+      parse
+        [ l 'A'
+        , l 'B'
+        , r [l 'C']
+        , r [l 'D', r [l 'E']]
+        ]
+        `shouldBe` Right [expect]
+
     specify "RAVs" do
       let inp :: [Simp Char]
           inp =
@@ -21,9 +78,6 @@ spec = do
             , r [l 'N', l 'O', r [l 'P']]
             , l 'Q'
             ]
-            where
-              l ch = Simp (Left ch)
-              r xs = Simp (Right xs)
           expect :: Tree Char
           expect =
             n
@@ -54,6 +108,4 @@ spec = do
                       ]
                   ]
               ]
-            where
-              n = Node
       parse inp `shouldBe` Right [expect]

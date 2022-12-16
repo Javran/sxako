a937b852113efab6f4919fce2bdfecd4282eab15
Javran Cheng <Javran.C@gmail.com>
2022-12-15 21:43:28 -0800

    move stuff around.

--- test/Game/Sxako/Pgn/Pass0Spec.hs
+++ test/Game/Sxako/Pgn/Pass0Spec.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE QuasiQuotes #-}
 
-module Game.Sxako.PgnSpec where
+module Game.Sxako.Pgn.Pass0Spec where
 
 import qualified Data.ByteString.Char8 as BSC
 import Game.Sxako.Common
-import Game.Sxako.Pgn
+import Game.Sxako.Pgn.Pass0
 import Test.Hspec
 import Test.Hspec.Attoparsec
 import Text.RawString.QQ
